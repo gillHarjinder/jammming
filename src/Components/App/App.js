c1c218f052f067eb9a8477256625db397efe0d9e
harjindergill\gill <laddivaliant@live.com>
2018-01-15 15:26:51 -0800

    interface is all setup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SearchBar from '../SearchBar/SearchBar';
+import Playlist from '../Playlist/Playlist';
+import SearchResults from '../SearchResults/SearchResults';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          //<!-- Add a SearchBar component -->
+          <SearchBar/>
           <div className="App-playlist">
-            //<!-- Add a SearchResults component -->
-            //<!-- Add a Playlist component -->
+            <SearchResults/>
+            <Playlist/>
           </div>
         </div>
       </div>
