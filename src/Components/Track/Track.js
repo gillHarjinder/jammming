9c8513b4ae6c216728888718b537587b6d4c4f5f
harjindergill\gill <laddivaliant@live.com>
2018-01-18 19:07:16 -0800

    fix some bugs

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,12 +3,24 @@ import './Track.css';
 
 
 class Track extends React.Component {
+
+	renderAction(){
+		if(isRemoval){
+			
+		}
+	}
+
 	render() {
 		return(
 			<div className="Track">
   				<div className="Track-information">
     				<h3> track name will go here </h3>
     				<p> track artist will go here |  track album will go here </p>
+  				</div>
+  				<a className="Track-action"> + or - will go here </a>
+  				<div className="Track-information">
+    				<h3> Tiny Dancer </h3>
+    				<p> Elton John |  Madman Across The Water </p>
   				</div>
   				<a className="Track-action"> + or - will go here </a>
 			</div>
